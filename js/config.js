2bae71aad758bd437045a883797d3855927fe252
Bai Li <bai.li@woodside.com.au>
2017-11-20 23:30:38 +0800

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,12 @@
+
+var _config = {
+    cognito: {
+        userPoolId: 'ap-southeast-2_8em7zqDT9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'god0oem4bt86ebnba4fgr1vbo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2', // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'Base URL of your API including the stage', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod'
+    }
+};
+    
